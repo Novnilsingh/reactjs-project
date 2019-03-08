3ca54659857a5cf937ddb52c45bd2bd87c6307a1
Novnilsingh <novnilsingh@gmail.com>
2019-03-08 14:05:40 -0600

    react-redux app

---
+++ src/userActions.js
@@ -0,0 +1,38 @@
+function fetchPostsRequest(){
+    return {
+        type: "FETCH_REQUEST"
+    }
+}
+
+function fetchPostsSuccess(payload) {
+    return {
+        type: "FETCH_SUCCESS",
+        payload
+    }
+}
+
+function fetchPostsError() {
+    return {
+        type: "FETCH_ERROR"
+    }
+}
+
+export function fetchPostsWithRedux() {
+    return (dispatch) => {
+        dispatch(fetchPostsRequest());
+        return fetchPosts().then(([response, json]) =>{
+            if(response.status === 200){
+                dispatch(fetchPostsSuccess(json))
+            }
+            else{
+                dispatch(fetchPostsError())
+            }
+        })
+    }
+}
+
+function fetchPosts() {
+    const URL = "http://dummy.restapiexample.com/api/v1/employees";
+    return fetch(URL, { method: 'GET'})
+        .then( response => Promise.all([response, response.json()]));
+}
